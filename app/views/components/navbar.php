3d58d7d47e7c643dca0003594e2c170e1287a2c4
Matheus Benediht <matheusbenediht@gmail.com>
2024-05-23 18:20:18 -0300

    Update: Estruturando projeto e atualizando componentes

---
+++ app/views/components/navbar.php
@@ -0,0 +1,36 @@
+<div class="navbar-area navbar-one">
+  <div class="container-fluid">
+    <div class="row">
+      <nav class="navbar bg-blue p-4">
+        <div class="mx-5 d-flex justify-content-start align-items-center">
+          <a href="#" class="navbar-brand">
+            <img
+              src="../../public/assets/imgs/logo.png"
+              width="355"
+              height="57"
+              alt="biotech"
+            />
+          </a>
+        </div>
+        <div class="d-flex justify-content-end">
+          <ul class="nav m-auto fs-5 align-items-center">
+            <li class="nav-item mx-2">
+              <a href="#" class="my-nav-link">Home</a>
+            </li>
+            <li class="nav-item mx-2">
+              <a href="#" class="my-nav-link">Sequencing</a>
+            </li>
+            <li class="nav-item mx-2">
+              <a href="#" class="my-nav-link">About us</a>
+            </li>
+            <li class="nav-item mx-2">
+              <a href="#" class="my-nav-link"
+                ><i class="bi bi-person" style="font-size: 2rem"></i
+              ></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
+</div>
---
+++ app/views/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>BioSequencer</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+    />
+    <link rel="stylesheet" href="../../public/assets/css/style.css" />
+  </head>
+  <body>
+    <?php include './components/navbar.php' ?>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12 d-flex justify-content-center bg-gray">
+          <div class="fs-4 m-5 p-4">
+            <h2 class="fw-medium">Lorem ipsum</h2>
+            <p class="fw-normal">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+              expedita voluptas culpa sapiente alias molestiae. Numquam corrupti
+              in laborum sed rerum et corporis.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 d-flex justify-content-center my-5">
+            <h2 class="fw-normal text-center">Lorem ipsum</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="d-flex justify-content-center">
+        <div class="col-md-3 ms-4">
+          <div class="card">
+            <div><i class="bi bi-database"></i></div>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 ms-4">
+          <div class="card">
+            <div class="card-header">Featured</div>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 ms-4">
+          <div class="card">
+            <div class="card-header">Featured</div>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
