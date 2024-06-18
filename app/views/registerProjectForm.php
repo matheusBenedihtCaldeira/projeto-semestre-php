46f494b5cb6cfdfacc9c937519789296da83cf0f
Matheus Benediht <matheusbenediht@gmail.com>
2024-06-18 17:22:00 -0300

    ultimos ajustes

--- app/views/registerProjectForm.php
+++ app/views/registerProjectForm.php
@@ -23,23 +23,23 @@
       <div class="row">
         <div class="col-md-12">
           <div class="mt-5">
-            <h2>Register Project</h2>
+            <h2>Registrar projeto</h2>
           </div>
           <div class="mt-5">
             <form action="../controllers/project/RegisterProjectController.php" enctype="multipart/form-data" method="post">
               <div class="row">
                 <div class="col-md-10 mb-3">
-                  <label for="name" class="form-label">Project Name</label>
+                  <label for="name" class="form-label">Nome do projeto</label>
                   <input type="text" name="name" class="form-control" />
                 </div>
               </div>
               <div class="row">
                 <div class="col-md-5 mb-3">
-                  <label for="start_date" class="form-label">Start Date</label>
+                  <label for="start_date" class="form-label">Inicio do projeto</label>
                   <input type="date" name="start_date" class="form-control" />
                 </div>
                 <div class="col-md-5 mb-3">
-                  <label for="end_date" class="form-label">End Date</label>
+                  <label for="end_date" class="form-label">Fim do projeto</label>
                   <input type="date" name="end_date" class="form-control" />
                 </div>
                 
@@ -48,7 +48,7 @@
                 <div class="col-md-10 mb-3">
                   <div class="form-floating">
                     <textarea class="form-control" placeholder="Leave a comment here" name="description"></textarea>
-                    <label for="description">Description</label>
+                    <label for="description">Detalhe do projeto</label>
                   </div>
                 </div>
               </div>
